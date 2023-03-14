955cf17fdb32479a8eb4c5ede5394b94d9ef2033
zburkard <burkard.exe@gmail.com>
2023-03-14 15:53:19 -0600

    chat page working. adding functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
+import Chat from "@/components/chat/index.jsx";
 
 function App() {
 
