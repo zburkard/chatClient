5af37c76d2ec7227df5807c36e9b78e9d8aa9e52
zburkard <burkard.exe@gmail.com>
2023-03-15 16:35:49 -0600

    handleSubmit working

--- src/components/customMessageForms/standardMessageForm.jsx
+++ src/components/customMessageForms/standardMessageForm.jsx
@@ -1,11 +1,25 @@
 import React, {useState} from 'react'
 import Dropzone from "react-dropzone";
-import {PaperClipIcon, XMarkIcon} from "@heroicons/react/24/solid"
-const StandardMessageForm = () => {
+import {PaperClipIcon, XMarkIcon, PaperAirplaneIcon} from "@heroicons/react/24/solid"
+const StandardMessageForm = ({props, activeChat}) => {
   const [message, setMessage] = useState("");
   const [attachment, setAttachment] = useState("")
   const [preview, setPreview] = useState("")
   const handleChange = (e) => setMessage(e.target.value)
+  const handleSubmit = async () => {
+    const date = new Date().toISOString().replace("T", " ").replace("Z", `${Math.floor(Math.random() * 1000)}+00:00`);
+    const attach = attachment ? [{ blob: attachment, file: attachment.name}] : [];
+    const form = {
+      attachments: attach,
+      created: date,
+      sender_username: props.username,
+      text: message,
+      activeChatId: activeChat.id
+    }
+    props.onSubmit(form);
+    setMessage("");
+    setAttachment("");
+  }
   return (
     <div className="message-form-container">{preview &&(<div className="message-form-preview">
       <img className="message-form-preview-image" src={preview} onLoad={()=> URL.revokeObjectURL(preview)} alt="" />
@@ -25,11 +39,16 @@ const StandardMessageForm = () => {
           }}>
             {({ getRootProps, getInputProps, open}) => (
               <div {...getRootProps()}>
-                <input {...getInputProps} />
-                <PaperClipIcon />
+                <input {...getInputProps()} />
+                <PaperClipIcon className="message-form-icon-clip" onClick={open}/>
               </div>
             )}
           </Dropzone>
+          <hr className="vertical-line"/>
+          <PaperAirplaneIcon className="message-form-icon-airplane" onClick={()=> {
+            setPreview("");
+            handleSubmit();
+          } }/>
         </div>
       </div>
     </div>
